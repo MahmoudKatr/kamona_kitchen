26b8b67e226cd5ceca32e4e655a10eece462b4cc
mahmoud <mahmoud.modo@gmail.com>
2024-06-23 02:56:41 +0300

    fetchSectionDataItem

--- lib/screen/SectionDetailScreen.dart
+++ lib/screen/SectionDetailScreen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class SectionDetailScreen extends StatelessWidget {
+class SectionDetailScreen extends StatefulWidget {
   final String sectionId;
   final String sectionName;
 
   SectionDetailScreen({required this.sectionId, required this.sectionName});
+
+  @override
+  _SectionDetailScreenState createState() => _SectionDetailScreenState();
+}
+
+class _SectionDetailScreenState extends State<SectionDetailScreen> {
+  late Map<String, dynamic> sectionData;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchSectionData();
+  }
+
+  Future<void> fetchSectionData() async {
+    final response = await http.get(Uri.parse('http://192.168.56.1:4000/admin/branch/sections/${widget.sectionId}'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        sectionData = json.decode(response.body);
+        isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to load section data');
+    }
+    
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,20 +44,22 @@ class SectionDetailScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Section ID: $sectionId',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Section Name: $sectionName',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
+        child: isLoading
+            ? Center(child: CircularProgressIndicator())
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Section ID: ${widget.sectionId}',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Section Name: ${sectionData['sectionName']}',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
       ),
     );
   }
